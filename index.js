493bff5b49b59176959e2cfd4c32cbb7f353abb7
wfx-oss <fxfmk7777@gmail.com>
2026-08-08 17:37:30 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
+
+client.login(process.env.TOKEN);
