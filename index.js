12d2f9fc9b743dd39cff85bf2791369cc145878a
wfx-oss <fxfmk7777@gmail.com>
2026-08-08 18:24:58 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const express = require('express');
+
+// 1. إنشاء سيرفر ويب وهمي لكي يرضى موقع Render ويترك البوت يعمل
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Bot is running!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
+
+// 2. كود بوت الدسكورد العادي
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('pong');
+    }
+});
+
+client.login(process.env.TOKEN);
